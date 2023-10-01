e235db8287e907a5b3f2ef8854425e2f7d0fb211
jayson <jayseip2004@gmail.com>
2023-09-30 21:30:58 -0400

    User class created

--- app/src/main/java/com/example/seg2105project/User.java
+++ app/src/main/java/com/example/seg2105project/User.java
@@ -1,4 +1,38 @@
 package com.example.seg2105project;
 
-public class User {
+public abstract class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String accountPassword;
+    private int phoneNumber;
+    private String address;
+    public User(String firstName, String lastName, String email, String accountPassword, int phoneNumber, String address){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.email=email;
+        this.accountPassword=accountPassword;
+        this.phoneNumber=phoneNumber;
+        this.address=address;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getAccountPassword(){
+        return this.accountPassword;
+    }
+    public int getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    public String getAddress(){
+        return this.accountPassword;
+    }
+
+
 }
