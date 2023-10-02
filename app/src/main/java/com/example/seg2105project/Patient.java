4a399b2c7bdff458d5ff0f882938793574ebabde
Michael Massaad <mikmas2004@icloud.com>
2023-10-02 12:27:15 -0400

    Patient class with all parameters, getter method and constructor

---
+++ app/src/main/java/com/example/seg2105project/Patient.java
@@ -0,0 +1,13 @@
+package com.example.seg2105project;
+public class Patient extends User{
+    private String healthCardNumber;
+
+    public Patient(String firstName, String lastName, String email, String accountPass, int phoneNumber, String address, String healthCardNum){
+        super(firstName, lastName, email, accountPass, phoneNumber, address);
+        this.healthCardNumber = healthCardNum;
+    }
+
+    public String getHealthCardNumber(){
+        return this.healthCardNumber;
+    }
+}
