c8bb678b8a0e1e41fbc685b9a25ce29af8c0fe4e
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-10-02 14:05:50 -0400

    Redo patient registration with activity and link with mainActivity

--- app/src/main/java/com/example/seg2105project/Registration.java
+++
@@ -1,9 +0,0 @@
-package com.example.seg2105project;
-
-public class Registration {
-
-    private String typeOfUser;
-
-    private User user;
-
-}
---
+++ app/src/main/java/com/example/seg2105project/RegistrationActivity.java
@@ -0,0 +1,21 @@
+package com.example.seg2105project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+    private String typeOfUser;
+
+    private User user;
+
+    public void createUser() {
+
+    }
+}
