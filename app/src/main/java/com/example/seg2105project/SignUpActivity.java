4747d984de1d63d75b8cdfa8e9ac7c2571c98f70
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-10-02 20:48:25 -0400

    Implement sign up page UI

--- app/src/main/java/com/example/seg2105project/SignUpActivity.java
+++ app/src/main/java/com/example/seg2105project/SignUpActivity.java
@@ -4,21 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 
     public void onClickButton(View view) {
         int pressID = view.getId();
-        if(pressID == R.id.signUpButton){
-            Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+        if(pressID == R.id.patientButton){
+            Intent i = new Intent(SignUpActivity.this, RegistrationActivityPatient.class);
+            startActivity(i);
+        } else if(pressID == R.id.doctorButton){
+            Intent i = new Intent(SignUpActivity.this, RegistrationActivityDoctor.class);
             startActivity(i);
         }
     }
