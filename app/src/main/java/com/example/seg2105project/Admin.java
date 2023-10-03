bcd0f342a9255e6ca4c6edbb03d1f1994b28a075
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-10-03 16:11:12 -0400

    Create Admin.java

---
+++ app/src/main/java/com/example/seg2105project/Admin.java
@@ -0,0 +1,8 @@
+package com.example.seg2105project;
+
+public class Admin extends User {
+    
+    public Admin(String firstName, String lastName, String email, String accountPass, int phoneNumber, String address) {
+        super(firstName, lastName, email, accountPass, phoneNumber, address);
+    }
+}
