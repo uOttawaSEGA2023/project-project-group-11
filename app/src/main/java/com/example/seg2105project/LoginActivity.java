5753d431920851f48f8a7f5c673e0d63d33b0f53
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-10-04 13:05:39 -0400

    Implement log in using Firebase authentication

--- app/src/main/java/com/example/seg2105project/LoginActivity.java
+++ app/src/main/java/com/example/seg2105project/LoginActivity.java
@@ -1,22 +1,41 @@
 package com.example.seg2105project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.*;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText username, password;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public void onClickLogin(){
+    public void onClickLogin(View view){
         username = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.logInPassword);
-        // use Firebase Authentication to implement authentication
+        mAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Logged in!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Failed to Log In!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
