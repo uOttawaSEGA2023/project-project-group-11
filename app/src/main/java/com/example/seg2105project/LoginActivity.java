d6261114d80ec95e01b550f4b73a006f09759a53
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-10-15 18:34:12 -0400

    Linting LoginActivity

--- app/src/main/java/com/example/seg2105project/LoginActivity.java
+++ app/src/main/java/com/example/seg2105project/LoginActivity.java
@@ -56,8 +56,12 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.logInPassword);
         // sign in the user using email and password
         // Note: code assisted by https://firebase.google.com/docs/auth/android/password-auth
-        // and https://stackoverflow.com/questions/47296771/firebase-how-to-get-child-of-child#:~:text=To%20solve%20this%2C%20you%20need%20to%20query%20your,rootRef.child%20%28%22users%22%29%3B%20Query%20regnoQuery%20%3D%20usersRef.orderByChild%20%28%22regno%22%29.equalsTo%20%28regno%29%3B
-        mAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString())
+        // and https://stackoverflow.com/questions/47296771/firebase-how-to-get-child-of-child#:~:
+        // text=To%20solve%20this%2C%20you%20need%20to%20query%20your,rootRef.child%20%28%22users%22
+        // %29%3B%20Query%20regnoQuery%20%3D%20usersRef.orderByChild%20%28%22regno%22%29.equalsTo%20
+        // %28regno%29%3B
+        mAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText()
+                        .toString())
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -66,63 +70,87 @@ public class LoginActivity extends AppCompatActivity {
                             // reference to "users" key in the database
                             DatabaseReference usersRef = databaseReference.child("users");
                             // getting every child that has an email equal to the username input
-                            Query usernameQuery =usersRef.orderByChild("email").equalTo(username.getText().toString());
+                            Query usernameQuery =usersRef.orderByChild("email")
+                                    .equalTo(username.getText().toString());
                             usernameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
 
                                 // retrieving user data that matches the username input
                                 @Override
                                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                                     try{
-                                        // loop through snapshot data of user obtained from match above
-                                        // NOTE: should only have to loop through 1 child since only one match
+                                        // loop through snapshot data of user obtained from match
+                                        // above
+                                        // NOTE: should only have to loop through 1 child since only
+                                        // one match
                                         for(DataSnapshot ds : snapshot.getChildren()) {
-                                            // create variables to create new user object based on user data
-                                            String firstName = ds.child("firstName").getValue(String.class);
-                                            String lastName = ds.child("lastName").getValue(String.class);
-                                            String email = ds.child("email").getValue(String.class);
-                                            String accountPassword = ds.child("accountPassword").getValue(String.class);
-                                            String phoneNumber = ds.child("phoneNumber").getValue(String.class);
+                                            // create variables to create new user object based on
+                                            // user data
+                                            String firstName = ds.child("firstName")
+                                                    .getValue(String.class);
+                                            String lastName = ds.child("lastName")
+                                                    .getValue(String.class);
+                                            String email = ds.child("email")
+                                                    .getValue(String.class);
+                                            String accountPassword = ds.child("accountPassword")
+                                                    .getValue(String.class);
+                                            String phoneNumber = ds.child("phoneNumber")
+                                                    .getValue(String.class);
 
                                             // receiving for address class
-                                            String postalAddress = ds.child("address").child("postalAddress").getValue(String.class);
-                                            String postalCode = ds.child("address").child("postalCode").getValue(String.class);
-                                            String city = ds.child("address").child("city").getValue(String.class);
-                                            String province = ds.child("address").child("province").getValue(String.class);
-                                            String country = ds.child("address").child("country").getValue(String.class);
+                                            String postalAddress = ds.child("address")
+                                                    .child("postalAddress").getValue(String.class);
+                                            String postalCode = ds.child("address")
+                                                    .child("postalCode").getValue(String.class);
+                                            String city = ds.child("address")
+                                                    .child("city").getValue(String.class);
+                                            String province = ds.child("address")
+                                                    .child("province").getValue(String.class);
+                                            String country = ds.child("address")
+                                                    .child("country").getValue(String.class);
                                             Address address = new Address(postalAddress, postalCode, city, province, country);
 
                                             String type = ds.child("type").getValue(String.class);
                                             // creating new objects based on above data
                                             switch (type) {
                                                 case "patient":
-                                                    String healthCardNumber = ds.child("healthCardNumber").getValue(String.class);
+                                                    String healthCardNumber = ds.child("healthCardNumber")
+                                                            .getValue(String.class);
                                                     if (healthCardNumber != null) {
-                                                        user = new Patient(firstName, lastName, email, accountPassword, phoneNumber, address, healthCardNumber);
+                                                        user = new Patient(firstName, lastName, email,
+                                                                accountPassword, phoneNumber, address, healthCardNumber);
                                                     }
                                                     break;
                                                 case "doctor":
-                                                    String employeeNumber = ds.child("employeeNumber").getValue(String.class);
-                                                    ArrayList<String> specialties = (ArrayList<String>) ds.child("specialties").getValue();
+                                                    String employeeNumber = ds.child("employeeNumber")
+                                                            .getValue(String.class);
+                                                    ArrayList<String> specialties = (ArrayList<String>)
+                                                            ds.child("specialties").getValue();
                                                     if (employeeNumber != null && specialties != null) {
-                                                        user = new Doctor(firstName, lastName, email, accountPassword, phoneNumber, address, employeeNumber, specialties);
+                                                        user = new Doctor(firstName, lastName, email,
+                                                                accountPassword, phoneNumber, address,
+                                                                employeeNumber, specialties);
                                                     }
                                                     break;
                                                 case "admin":
-                                                    user = new Admin(firstName, lastName, email, accountPassword, phoneNumber, address);
+                                                    user = new Admin(firstName, lastName, email,
+                                                            accountPassword, phoneNumber, address);
                                                     break;
                                             }
 
                                             // put user object into bundle and go to welcome page
-                                            Intent i = new Intent(LoginActivity.this, WelcomePageActivity.class);
+                                            Intent i = new Intent(LoginActivity.this,
+                                                    WelcomePageActivity.class);
                                             i.putExtra("User", user);
                                             i.putExtra("Type", type);
                                             startActivity(i);
                                             // toast to tell user that log in is successful
-                                            Toast.makeText(LoginActivity.this, "Logged in!", Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(LoginActivity.this, "Logged in!",
+                                                    Toast.LENGTH_SHORT).show();
                                         }
                                     }catch(Exception e){
                                         // catch exception if error occurs retrieving data
-                                        Toast.makeText(LoginActivity.this, "Failed to retrieve user data!", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(LoginActivity.this, "Failed to retrieve user data!",
+                                                Toast.LENGTH_SHORT).show();
                                         System.out.println(e);
                                     }
 
@@ -136,7 +164,8 @@ public class LoginActivity extends AppCompatActivity {
                             });
                         } else {
                             // user failed to log in based on wrong credentials
-                            Toast.makeText(LoginActivity.this, "Username or password is incorrect!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "Username or password is incorrect!",
+                                    Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
