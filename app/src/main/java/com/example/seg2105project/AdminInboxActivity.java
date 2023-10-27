26408098c3750fe0ea234e64c01acda3d77adead
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-10-27 13:42:12 -0400

    Implement different tab display on admin inbox

--- app/src/main/java/com/example/seg2105project/AdminInboxActivity.java
+++ app/src/main/java/com/example/seg2105project/AdminInboxActivity.java
@@ -6,23 +6,68 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 
 public class AdminInboxActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_inbox);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        // get tablayout
+        tabLayout = findViewById(R.id.requestList);
 
+        // get recycler view of lists
+        recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         // display pending list
         RegistrationRequestManager.getList("pending", new SimpleCallback<ArrayList<User>>() {
             @Override
             public void callback(ArrayList<User> data) {
                 recyclerView.setAdapter(new ListAdapter(getApplicationContext(), data));
+
+            }
+        });
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                if(position == 0){
+                    // display pending list
+                    RegistrationRequestManager.getList("pending", new SimpleCallback<ArrayList<User>>() {
+                        @Override
+                        public void callback(ArrayList<User> data) {
+                            recyclerView.setAdapter(new ListAdapter(getApplicationContext(), data));
+
+                        }
+                    });
+                }
+                else if(position == 1){
+                    // display rejected list
+                    RegistrationRequestManager.getList("rejected", new SimpleCallback<ArrayList<User>>() {
+                        @Override
+                        public void callback(ArrayList<User> data) {
+                            recyclerView.setAdapter(new ListAdapter(getApplicationContext(), data));
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
     }
