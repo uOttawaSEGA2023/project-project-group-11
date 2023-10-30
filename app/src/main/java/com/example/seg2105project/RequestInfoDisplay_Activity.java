8c45c4124abee89ae16f48faf422fd0d98b2a847
Christian Sampson <csamp013@uottawa.ca>
2023-10-29 20:48:55 -0400

    Changed Activity_Request further

--- app/src/main/java/com/example/seg2105project/RequestInfoDisplay_Activity.java
+++ app/src/main/java/com/example/seg2105project/RequestInfoDisplay_Activity.java
@@ -1,18 +1,58 @@
 package com.example.seg2105project;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.view.View;
+import android.content.Intent;
+
+
+
 
 /**
  * The RequestInfoDisplay_Activity class represents an activity for displaying information about a registration request.
  * It provides a user interface for viewing details related to a registration request.
  */
 public class RequestInfoDisplay_Activity extends AppCompatActivity {
-
+    TextView userType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_info_display);
+
+        String type = getIntent().getExtras().getString("Type");
+
+        userType = findViewById(R.id.userType);
+        userType.setText(type+ "Registration Overview");
+
+        LinearLayout specialties, employeeNumber, healthCardNumber;
+        specialties = findViewById(R.id.specialties);
+        employeeNumber = findViewById(R.id.employeeNumber);
+        healthCardNumber = findViewById(R.id.employeeNumber);
+        if(type.equals("doctor")){
+            specialties.setVisibility(View.VISIBLE);
+            employeeNumber.setVisibility(View.VISIBLE);
+            healthCardNumber.setVisibility(View.INVISIBLE);
+        }
+        else{
+            specialties.setVisibility(View.INVISIBLE);
+            employeeNumber.setVisibility(View.INVISIBLE);
+            healthCardNumber.setVisibility(View.VISIBLE);
+        }
     }
+    public void onClick(View view){
+        if(view.getId() == R.id.backtoInbox){
+            Intent backtoInbox = new Intent(RequestInfoDisplay_Activity.this, AdminInboxActivity.class);
+            startActivity(backtoInbox);
+        }
+        else if(view.getId() == R.id.acceptUser){
+
+        }
+        else if(view.getId() == R.id.rejectUser){
+
+        }
+    }
+
 }
