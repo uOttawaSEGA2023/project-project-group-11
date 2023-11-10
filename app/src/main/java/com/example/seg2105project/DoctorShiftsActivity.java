fe132de24f10102402ddfea58bf4d8d2c95ac7d4
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-11-09 20:54:17 -0500

    Update Doctor shift class with unimplemented methods

--- app/src/main/java/com/example/seg2105project/DoctorShiftsActivity.java
+++ app/src/main/java/com/example/seg2105project/DoctorShiftsActivity.java
@@ -27,11 +27,15 @@ public class DoctorShiftsActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     TabLayout tabLayout;
 
+    // shift list instance
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_shifts);
+
+        // get database instances
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference();
         mAuth = FirebaseAuth.getInstance();
@@ -49,7 +53,9 @@ public class DoctorShiftsActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // if there are shifts, display them
                 if(snapshot.child("shifts").exists()){
-                    // display shifts
+                    // get list of shifts from the database
+                    // update shift list instance with list of shifts from database
+                    // display shifts using new item_view xml, ListViewHolder, and ListAdapter
                 }
             }
 
@@ -59,4 +65,37 @@ public class DoctorShiftsActivity extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * Creates an Alert Dialog to add a shift
+     */
+    public void createAlertDialog(){
+        // create alert dialog
+        // if using calendar, check if specified date and time has passed and prevent from being clicked
+        // Update the start-time and end-time so that they are 30-minute incremented every time one is updated
+        // call addShift()
+    }
+
+    /**
+     * Adds a shift for a Doctor object.
+     */
+    public void addShift() {
+        // get data from Views
+        // If not using calendar, check if the specified date and time that the Doctor wants to add has passed
+        // Check double booking (shift being added must not conflict with an existing one)
+        // create a Shift object
+        // add Shift object to shift list instance
+        // update list in database
+    }
+
+    /**
+     * Deletes a shift for a Doctor object.
+     *
+     */
+    public void deleteShift() {
+        // get location of shift that has been clicked
+        // delete shift from shift list
+        // update list in database
+    }
+
 }
