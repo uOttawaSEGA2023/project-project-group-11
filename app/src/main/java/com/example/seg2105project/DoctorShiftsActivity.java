1c9d74ecd058d61a9a2756f460bfe12431c8a63b
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-11-11 16:17:28 -0500

    Add alert dialog and calendar for adding shift

--- app/src/main/java/com/example/seg2105project/DoctorShiftsActivity.java
+++ app/src/main/java/com/example/seg2105project/DoctorShiftsActivity.java
@@ -1,11 +1,18 @@
 package com.example.seg2105project;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -60,8 +67,43 @@ public class DoctorShiftsActivity extends AppCompatActivity {
     /**
      * Creates an Alert Dialog to add a shift
      */
-    public void createAlertDialog(){
+    public void createAlertDialog(View view){
+
         // create alert dialog
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.add_shift_layout, null);
+        dialogBuilder.setView(dialogView);
+
+        // get data from view elements
+        final CalendarView calendar = (CalendarView) dialogView.findViewById(R.id.calendar);
+        TextView selectedDate = (TextView) dialogView.findViewById(R.id.selectedDate);
+        final EditText editTextStartTime  = (EditText) dialogView.findViewById(R.id.editTextStartTime);
+        final EditText editTextEndTime  = (EditText) dialogView.findViewById(R.id.editTextEndTime);
+        final Button buttonAdd = (Button) dialogView.findViewById(R.id.buttonAddShift);
+
+        // display alert dialog
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        // set data for calendar
+        calendar.setOnDateChangeListener(new CalendarView
+                .OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                String date = (month + 1) + "-" + day + "-" + year;
+                selectedDate.setText("Date Chosen: " + date);
+            }
+        });
+
+        // add a shift when button is clicked
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addShift();
+            }
+        });
+
         // if using calendar, check if specified date and time has passed and prevent from being clicked
         // Update the start-time and end-time so that they are 30-minute incremented every time one is updated
         // call addShift()
