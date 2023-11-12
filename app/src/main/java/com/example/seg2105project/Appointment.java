fcd9790be8c2625ea4cfe9e319fb0d99c11662bb
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-11-12 12:22:02 -0500

    Update constructors of Appointment And Shift

--- app/src/main/java/com/example/seg2105project/Appointment.java
+++ app/src/main/java/com/example/seg2105project/Appointment.java
@@ -2,14 +2,15 @@ package com.example.seg2105project;
 
 
 public class Appointment {
+
     private Patient patient;
     private Doctor doctor;
     private String status;
     private String date;
-
     private String startTime;
     private String endTime;
-    Appointment(Patient patient, Doctor doctor, String status,String date, String startTime, String endTime){
+
+    public Appointment(Patient patient, Doctor doctor, String status,String date, String startTime, String endTime){
         this.patient = patient;
         this.doctor=doctor;
         this.status=status;
