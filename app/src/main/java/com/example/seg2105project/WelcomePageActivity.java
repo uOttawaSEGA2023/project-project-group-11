9ad10bb200adca161e952460577428719153cfe8
Michael Massaad <mikmas2004@icloud.com>
2023-11-12 21:20:22 -0500

    Modifying the buttons to the appointments

--- app/src/main/java/com/example/seg2105project/WelcomePageActivity.java
+++ app/src/main/java/com/example/seg2105project/WelcomePageActivity.java
@@ -96,11 +96,17 @@ public class WelcomePageActivity extends AppCompatActivity implements View.OnCli
                 Toast.makeText(WelcomePageActivity.this, "Error showing shifts", Toast.LENGTH_SHORT).show();
                 System.out.println(e);
             }
-        }/*
+        }
         else if(view.getId() == R.id.appointmentbutton){
             try{
-                Intent appointments = new Intent(WelcomePageActivity.this, )
+                Intent appointments = new Intent(WelcomePageActivity.this, DoctorAppointmentsActivity.class);
+                appointments.putExtra("User", user);
+                startActivity(appointments);
             }
-        }*/
+            catch(Exception e){
+                Toast.makeText(WelcomePageActivity.this, "Error showing appointments", Toast.LENGTH_SHORT).show();
+                System.out.println(e);
+            }
+        }
     }
 }
