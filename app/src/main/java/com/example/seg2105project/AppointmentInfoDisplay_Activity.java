494f6f4a63703046a9c87d932de349d301917bac
Tanner Frisch <tfrisch06@icloud.com>
2023-11-14 22:37:20 -0500

    updated appointment info display with database

--- app/src/main/java/com/example/seg2105project/AppointmentInfoDisplay_Activity.java
+++ app/src/main/java/com/example/seg2105project/AppointmentInfoDisplay_Activity.java
@@ -1,9 +1,13 @@
 package com.example.seg2105project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -15,6 +19,8 @@ import java.util.Date;
 public class AppointmentInfoDisplay_Activity extends AppCompatActivity {
     TextView patientName, address, healthCardNumber, appointmentDate, appointmentStartTime, appointmentEndTime, status;
     Appointment appointment;
+
+    User user;
 
     // SimpleDateFormat for parsing time
     private static final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
@@ -42,6 +48,8 @@ public class AppointmentInfoDisplay_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment_info_display);
+
+        user = (User) getIntent().getExtras().getSerializable("User");
 
         // Retrieve appointment object from intent
         appointment = (Appointment) getIntent().getExtras().getSerializable("Appointment");
@@ -75,49 +83,71 @@ public class AppointmentInfoDisplay_Activity extends AppCompatActivity {
 
     /**
      * Handle the click event for accept/reject buttons.
+     *
      * @param view The clicked view.
      */
     public void onAcceptRejectClick(View view) {
         if (view.getId() == R.id.acceptAppointmentButton) {
-            // do something after appointment is accepted
-            appointment.setStatus(AppointmentStatus.ACCEPTED.getStatusText());
+            handleAppointmentAction("accepted", "User Accepted!", view);
         } else if (view.getId() == R.id.rejectAppointmentButton) {
-            // does something if appointment is rejected
-            appointment.setStatus(AppointmentStatus.REJECTED.getStatusText());
+            handleAppointmentAction("rejected", "User Rejected!", view);
         }
+
         // Update the UI after handling the click event
         updateUI();
     }
 
-    /**
-     * Check if an appointment is in the past.
-     * @param appointment The appointment to check.
-     * @return True if the appointment is in the past, false otherwise.
-     */
-    public static boolean isPastAppointment(Appointment appointment) {
-        try {
-            String dateTimeString = appointment.getDate() + " " + appointment.getStartTime();
-            Date appointmentDateTime = timeFormat.parse(dateTimeString);
+    private void handleAppointmentAction(String action, String toastMessage, View view) {
+        RegistrationRequestManager.transferData(user, action, "users", new SimpleCallback<String>() {
+            @Override
+            public void callback(String data) {
+                Toast.makeText(AppointmentInfoDisplay_Activity.this, toastMessage, Toast.LENGTH_SHORT).show();
+                navigateToAdminInbox();
+            }
+        });
 
-            Calendar currentCalendar = Calendar.getInstance();
-            Date currentDate = currentCalendar.getTime();
-
-            return appointmentDateTime.before(currentDate);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return false;
+        if (view.getId() == R.id.acceptAppointmentButton) {
+            appointment.setStatus(AppointmentStatus.ACCEPTED.getStatusText());
+        } else if (view.getId() == R.id.rejectAppointmentButton) {
+            appointment.setStatus(AppointmentStatus.REJECTED.getStatusText());
         }
     }
 
-    /**
-     * Update the UI to reflect changes.
-     */
-    private void updateUI() {
-        // Update the UI to reflect the changes in the appointment status
-        status.setText(appointment.getStatus().toString());
-
-        // Update visibility of buttons based on the status
-        findViewById(R.id.acceptAppointmentButton).setVisibility(View.GONE);
-        findViewById(R.id.rejectAppointmentButton).setVisibility(View.GONE);
+    private void navigateToAdminInbox() {
+        Intent backtoInbox = new Intent(AppointmentInfoDisplay_Activity.this, AdminInboxActivity.class);
+        startActivity(backtoInbox);
     }
-}
+
+
+    /**
+             * Check if an appointment is in the past.
+             * @param appointment The appointment to check.
+             * @return True if the appointment is in the past, false otherwise.
+             */
+            public static boolean isPastAppointment (Appointment appointment){
+                try {
+                    String dateTimeString = appointment.getDate() + " " + appointment.getStartTime();
+                    Date appointmentDateTime = timeFormat.parse(dateTimeString);
+
+                    Calendar currentCalendar = Calendar.getInstance();
+                    Date currentDate = currentCalendar.getTime();
+
+                    return appointmentDateTime.before(currentDate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+
+            /**
+             * Update the UI to reflect changes.
+             */
+            private void updateUI () {
+                // Update the UI to reflect the changes in the appointment status
+                status.setText(appointment.getStatus().toString());
+
+                // Update visibility of buttons based on the status
+                findViewById(R.id.acceptAppointmentButton).setVisibility(View.GONE);
+                findViewById(R.id.rejectAppointmentButton).setVisibility(View.GONE);
+            }
+        }
