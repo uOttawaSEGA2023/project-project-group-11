faff16cf3cf45f4aeea3c3a0bac37d4c7217253b
Tanner Frisch <tfrisch06@icloud.com>
2023-11-27 19:11:00 -0500

    merge of Jayson and I's work. added rate doctor screen

--- app/src/main/java/com/example/seg2105project/PatientAppointmentInfoDisplay_Activity.java
+++ app/src/main/java/com/example/seg2105project/PatientAppointmentInfoDisplay_Activity.java
@@ -1,21 +1,32 @@
 package com.example.seg2105project;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TextView;
-
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-import android.view.View;
-
+/**
+ * Activity class to display appointment information for patients.
+ */
 public class PatientAppointmentInfoDisplay_Activity extends AppCompatActivity {
-    TextView doctorName, healthCardNumber, appointmentDate, appointmentStartTime, appointmentEndTime, status;
+    TextView doctorName, appointmentDate, appointmentStartTime, appointmentEndTime, doctorSpecialities;
     Appointment appointment;
 
     User user;
@@ -48,34 +59,114 @@ public class PatientAppointmentInfoDisplay_Activity extends AppCompatActivity {
         databaseReference = database.getReference();
         mAuth = FirebaseAuth.getInstance();
 
-        // Initializes TextViews
+        // Initialize TextViews
         doctorName = findViewById(R.id.doctor);
-        healthCardNumber = findViewById(R.id.healthCardNumber);
+        doctorSpecialities = findViewById(R.id.specialties);
         appointmentDate = findViewById(R.id.appointmentDate);
         appointmentStartTime = findViewById(R.id.appointmentStartTime);
         appointmentEndTime = findViewById(R.id.appointmentEndTime);
-        status = findViewById(R.id.status);
 
-        // Sets text views
-        doctorName.setText(appointment.getPatient().getFullName());
-        healthCardNumber.setText(appointment.getPatient().getHealthCardNumber());
-        appointmentDate.setText(appointment.getDate());
-        appointmentStartTime.setText(appointment.getStartTime());
-        appointmentEndTime.setText(appointment.getEndTime());
-        status.setText(appointment.getStatus());
+        // Set text for appointment details dynamically
+        doctorName.setText("Doctor: " + appointment.getDoctor().getFullName());
+        doctorSpecialities.setText("Specialities" + appointment.getDoctor().getSpecialties().toString());
+        appointmentDate.setText("Date: " + appointment.getDate());
+        appointmentStartTime.setText("Start Time: " + appointment.getStartTime());
+        appointmentEndTime.setText("End Time: " + appointment.getEndTime());
 
+        Button backButton = findViewById(R.id.backButton);
+        Button cancelAppointmentButton = findViewById(R.id.cancelAppointmentButton);
+        Button rateDoctorButton = findViewById(R.id.rateDoctorButton);
 
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
+        cancelAppointmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCancelButtonClick(v);
+            }
+        });
+
+        rateDoctorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRateDoctorButtonClick(v);
+            }
+        });
     }
 
-    public void OnClick(View view){
-        Intent intent = new Intent(PatientAppointmentInfoDisplay_Activity.this,
-                PatientAppointmentActivity.class);
+    public void onCancelButtonClick(View view) {
+        if (!isAppointmentWithin60Minutes(appointment.getStartTime())) {
+            // TODO: Implement cancel logic
 
-        if(view.getId()==R.id.backButton){
-            startActivity(intent);
+            // Display a toast message
+            Toast.makeText(this, "Appointment canceled!", Toast.LENGTH_SHORT).show();
+        } else {
+            // Display a toast message indicating that cancellation is not allowed
+            Toast.makeText(this, "Cannot cancel appointment within 60 minutes!", Toast.LENGTH_SHORT).show();
         }
+    }
 
+    public static boolean isAppointmentWithin60Minutes(String appointmentTime) {
+        try {
+            Date appointmentDateTime = timeFormat.parse(appointmentTime);
+
+            Calendar currentCalendar = Calendar.getInstance();
+            Date currentDate = currentCalendar.getTime();
+
+            long timeDifferenceMillis = appointmentDateTime.getTime() - currentDate.getTime();
+
+            long timeDifferenceMinutes = timeDifferenceMillis / (60 * 1000);
+
+            return timeDifferenceMinutes <= 60;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void onRateDoctorButtonClick(View view) {
+        // Retrieve doctor ID and rating value
+        String doctorId = appointment.getDoctor().getEmployeeNumber();
+        RatingBar ratingBar = findViewById(R.id.doctorRatingBar);
+        float ratingValue = ratingBar.getRating();
+
+        // Save the rating to the database
+        saveDoctorRating(doctorId, ratingValue);
+    }
+
+    private void saveDoctorRating(String doctorId, float ratingValue) {
+        // Assuming you have a "doctors" node in your database
+        DatabaseReference doctorRef = databaseReference.child("doctors").child(doctorId);
+
+        // Retrieve existing ratings and count from the database
+        doctorRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    long totalRatings = dataSnapshot.child("totalRatings").getValue(Long.class);
+                    float averageRating = dataSnapshot.child("averageRating").getValue(Float.class);
+
+                    // Update total ratings and calculate new average rating
+                    totalRatings++;
+                    averageRating = ((averageRating * (totalRatings - 1)) + ratingValue) / totalRatings;
+
+                    // Update the database with the new ratings
+                    doctorRef.child("totalRatings").setValue(totalRatings);
+                    doctorRef.child("averageRating").setValue(averageRating);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
     }
 
 
