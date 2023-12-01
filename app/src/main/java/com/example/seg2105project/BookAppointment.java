f63da86c6e1e2ecdcb0ac78c979b88e163cdb3d4
Michael Massaad <mikmas2004@icloud.com>
2023-12-01 11:45:12 -0500

    modifying layout file and adding search bar implementation start

--- app/src/main/java/com/example/seg2105project/BookAppointment.java
+++ app/src/main/java/com/example/seg2105project/BookAppointment.java
@@ -1,17 +1,26 @@
 package com.example.seg2105project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.SearchView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class BookAppointment extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_appointment);
+
+        SearchView searchSpecialty = findViewById(R.id.specialtySearch);
+        searchSpecialty.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            public boolean onQueryTextSubmit(String query){
+                handleSearch(query);
+                return true;
+            }
+        }
     }
 
     public void onClick(View view){
